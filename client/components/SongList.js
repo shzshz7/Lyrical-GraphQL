0ce605e9bf4fd679b9a7f2a52cf5f9f67ed7c6c6
Harrison <harrison@Harrisons-MacBook-Pro.local>
2018-05-01 11:08:44 -0400

    try commit from mac

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -17,7 +17,7 @@ class SongList extends Component{
     render() {
         // console.log(this.props);
         if (this.props.data.loading){
-            return <div>Loading...</div>
+            return <div>Loading...!</div>
         }
 
         return (
