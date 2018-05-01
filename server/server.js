5b735434af3e3d780896ceade8dcda182d01c453
shzshz777 <shzshz777@gmail.com>
2018-04-30 21:51:54 -0400

    query component bond

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://shzshz777:12041204@ds263759.mlab.com:63759/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
