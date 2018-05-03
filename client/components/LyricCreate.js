c84320cb2dd47ab0e74ed076a4fbf8127c9e061b
Harrison <shzshz777@gmail.com>
2018-05-03 08:32:56 -0400

    re-commit LyricCreate

---
+++ client/components/LyricCreate.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import {graphql} from 'react-apollo';
+import gql from 'graphql-tag';
+import fetchSong from '../queries/fetchSong';
+
+class LyricCreate extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            content:''
+        }
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+
+        this.props.mutate({
+            variables:{
+                content: this.state.content,
+                songId: this.props.songId
+            }
+            // ,refetchQueries:[{
+            //     query:gql`
+            //     {
+            //         song(id:"${this.props.songId}"){
+            //         id
+            //         title
+            //         lyrics {
+            //             id
+            //             content
+            //         }
+            //         }
+            //     }
+            // `
+            // }]
+        }).then(() => this.setState({content: ''}));
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <label>Add a Lyric</label>
+                <input
+                    value = {this.state.content}
+                    onChange={event => this.setState({content:event.target.value})}
+                />
+            </form>
+        );
+    }
+}
+
+const mutation = gql`
+    mutation AddLyricToSong($content:String, $songId:ID){
+        addLyricToSong(content:$content, songId:$songId){
+            id
+            lyrics {
+                id
+                content
+            }
+        }
+    }
+`
+
+export default graphql(mutation)(LyricCreate);
